5eeec7dbb96ec3e62ca20ed8093aa62954490ca5
chepito777pardo <chepito7777777@gmail.com>
2025-04-10 16:02:53 -0500

    se crea modelo de producto y hibernate

---
+++ src/main/java/com/barleague/models/Producto.java
@@ -0,0 +1,76 @@
+package com.barleague.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private int id;
+
+    @Column(nullable = false)
+    private String codigo;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(name = "valor_unitario", nullable = false)
+    private double valorUnitario;
+
+    // === CONSTRUCTORES ===
+    public Producto() {}
+
+    public Producto(String codigo, String nombre, String descripcion, double valorUnitario) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.valorUnitario = valorUnitario;
+    }
+
+    // === GETTERS Y SETTERS ===
+    public int getId() {
+        return id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+}
+
+
+
+
