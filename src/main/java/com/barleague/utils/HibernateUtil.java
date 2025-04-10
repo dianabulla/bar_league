7f61002b53cead4c3dd525f677b66ab1faaec2af
chepito777pardo <chepito7777777@gmail.com>
2025-04-10 16:09:47 -0500

    se crea hibernateutil y compradao

---
+++ src/main/java/com/barleague/utils/HibernateUtil.java
@@ -0,0 +1,23 @@
+package com.barleague.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Carga configuración desde hibernate.cfg.xml
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Error al crear la SessionFactory: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
